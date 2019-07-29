fcd32d83ef73071f2dd2a356e55151b124e6fc92
Eliza Munroe <era3munroe@gmail.com>
2019-07-29 14:12:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   string.split
   new_array = [ ]
-  new_array.each do |word|
-    word.reverse
+  array.each do |word|
+    new_array << word.reverse
   end
   new_array.join(" ")
 end
